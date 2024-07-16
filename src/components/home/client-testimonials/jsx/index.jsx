a12011c801b65a7f30ca14a00dc834d790c31e7f
Nandini <nandiniverma202021@gmail.com>
2024-07-16 14:26:00 +0530

    feat: add jsx folder for client-testimonial

---
+++ src/components/home/client-testimonials/jsx/index.jsx
@@ -0,0 +1,10 @@
+const stars = ()=> {
+    {
+        let stars = [];
+        for (let i = 0; i < 5; i++) {
+            stars.push(<img alt="star_icon" src="/icons/star-icon.svg"/>);
+        }
+        return stars; 
+    };
+}
+export default stars;
