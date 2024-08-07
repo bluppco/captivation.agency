65ecb2c1738b824d3c93b23bd1c5afdcdc026d26
Shiva pandey <shivapandey9898@gmail.com>
2024-08-07 13:41:52 +0530

    feat: component input  header jsx

---
+++ src/components/common/header/input/jsx/index.jsx
@@ -0,0 +1,10 @@
+const InputJsx = ( { placeholder } ) => (
+
+    <input 
+        className="px-4 py-2 border border-white bg-transparent placeholder:text-white focus:outline-none" 
+        placeholder={ placeholder }
+        type="text" 
+    />
+
+)
+export default InputJsx
